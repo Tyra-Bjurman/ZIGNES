877e7421fb5ee97cc9db107e25529a4407456c28
Tyra Bjurman <144486+Tyra-Bjurman@users.noreply.github.com>
2024-06-26 22:46:35 +0200

    Update cpu.zig

--- src/cpu.zig
+++ src/cpu.zig
@@ -1,19 +1,18 @@
 // Central Processing Unit
 
-const pub ProcessorStatusFlags = packed struct {
-  carry:                u1 = 0,    // Result carry bit
-  zero:                 u1 = 0,    // Result is zero
-  disable_interrupts:   u1 = 0,    
-  decimal_mode:         u1 = 0,    // Not used in NES
-  break_call:           u1 = 0,    // Break called by interrupt
-  unused:               u1 = 0,    // 
-  overflow:             u1 = 0,    // Result overflow 
-  negative:             u1 = 0,    // Negative result
+const ProcessorStatusFlags = packed struct {
+  carry: u1 = 0,               // Result carry bit
+  zero: u1 = 0,                // Result is zero
+  disable_interrupts: u1 = 0,    
+  decimal_mode: u1 = 0,        // Not used in NES
+  break_call: u1 = 0,          // Break called by interrupt
+  unused: u1 = 0,     
+  overflow: u1 = 0,            // Result overflow 
+  negative: u1 = 0,            // Negative result
 };
 
-var cpu_status = ProcessorStatusFlags;
-
 // 6502 CPU registers
+var cpu_status = ProcessorStatusFlags;
 var cpu_accumulator: u8;
 var x_register: u8;
 var y_register: u8;
