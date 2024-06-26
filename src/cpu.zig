7536879f0ec310d7de7b68856bee388724d49200
Tyra Bjurman <144486+Tyra-Bjurman@users.noreply.github.com>
2024-06-26 23:17:27 +0200

    Update cpu.zig

--- src/cpu.zig
+++ src/cpu.zig
@@ -1,23 +1,23 @@
 // Central Processing Unit
 
 const ProcessorStatusFlags = packed struct {
-  carry: u1 = 0,               // Result carry bit
-  zero: u1 = 0,                // Result is zero
-  disable_interrupts: u1 = 0,    
-  decimal_mode: u1 = 0,        // Not used in NES
-  break_call: u1 = 0,          // Break called by interrupt
-  unused: u1 = 0,     
-  overflow: u1 = 0,            // Result overflow 
-  negative: u1 = 0,            // Negative result
+  carry: u1,               // Result carry bit
+  zero: u1,                // Result is zero
+  disable_interrupts: u1,    
+  decimal_mode: u1,        // Not used in NES
+  break_call: u1,          // Break called by interrupt
+  unused: u1,     
+  overflow: u1,            // Result overflow 
+  negative: u1,            // Negative result
 };
 
 // 6502 CPU registers
-var status = ProcessorStatusFlags;
-var accumulator: u8;
-var x_register: u8;
-var y_register: u8;
-var stack_pointer: u8;
-var program_counter: u16;
+var status: ProcessorStatusFlags = 0x00;
+var accumulator: u8 = 0x00;
+var x_register: u8 = 0x00;
+var y_register: u8 = 0x00;
+var stack_pointer: u8 = 0x00;
+var program_counter: u16 = 0x0000;
 
 // Adressing modes
 fn ABS() u8;
@@ -30,4 +30,4 @@ fn XXX() u8;
 // Emulator helper functions
 var address_absolute: u16 = 0x0000;
 var address_relative: u16 = 0x0000;
-var opcode: u8 = 0;
+var opcode: u8 = 0x00;
